e21be354c50336c7123af0a2435accdf7d188bb1
Aiffah Kiysa Waafi <aiffahwaafi6@gmail.com>
2022-09-20 23:29:20 +0700

    angka 0

--- main.js
+++ main.js
@@ -3,10 +3,37 @@ function main() {
     var gl = kanvas.getContext("webgl");
 
     var vertices = [
-        0.5, 0.5, // x1, y1 kanan atas
-        0.0, 0.0, // x2, y2 bawah tengah
-        -0.5, 0.5, // x3, y3 kiri atas
-        0.0, 1.0 // atas tengah
+        // angka 0
+        -0.8, 0.6,
+        -0.7, 0.7,
+        -0.4, 0.7,
+        -0.3, 0.6,
+        -0.3, 0.2,
+        -0.4, 0.1,
+        -0.7, 0.1,
+        -0.8, 0.2,
+
+        -0.7, 0.6,
+        -0.4, 0.6,
+        -0.4, 0.2,
+        -0.7, 0.2,
+
+        // angka 2
+        0.0, 0.2,
+        0.0, 0.1,
+        0.5, 0.1,
+        0.5, 0.2,
+        0.15, 0.2,
+        0.5, 0.4,
+        0.5, 0.6,
+        0.4, 0.7,
+        0.1, 0.7,
+        0.0, 0.6,
+        0.0, 0.45,
+        0.12, 0.45,
+        0.12, 0.6,
+        0.4, 0.6,
+        0.4, 0.45
     ];
 
     var buffer = gl.createBuffer();
@@ -16,10 +43,11 @@ function main() {
     // Vertex shader
     var vertexShaderCode = `
     attribute vec2 aPosition;
+    // attribute vec3 aColor;
+    // varying vec3 fragColor;
     void main(){
-        float x = aPosition.x;
-        float y = aPosition.y;
-        gl_Position = vec4(x, y, 0.0, 1.0);
+        // fragColor = aColor;
+        gl_Position = vec4(aPosition.xy, 0.0, 1.0);
         gl_PointSize = 10.0;
     }
     `;
@@ -31,10 +59,11 @@ function main() {
     // Fragment shader
     var fragmentShaderCode = `
     precision mediump float;
+    // varying vec3 fragColor;
     void main() {
-        float r = 0.0;
+        float r = 1.0;
         float g = 0.0;
-        float b = 1.0;
+        float b = 0.0;
         gl_FragColor = vec4(r, g, b, 1.0);
     }
     `;
@@ -51,11 +80,18 @@ function main() {
     // mengajari GPU bagaimana cara mengoleksi nilai posisi dari ARRAY_BUFFER
     // untuk setiap vertex yang sedang diproses
     var aPosition = gl.getAttribLocation(shaderProgram, "aPosition");
+    // var aColor = gl.getAttribLocation(shaderProgram, "aColor");
     gl.vertexAttribPointer(aPosition, 2, gl.FLOAT, false, 0, 0);
+    // gl.vertexAttribPointer(aColor, 3, gl.FLOAT, false, 5 * Float32Array.BYTES_PER_ELEMENT, 2 * Float32Array.BYTES_PER_ELEMENT);
     gl.enableVertexAttribArray(aPosition);
+    // gl.enableVertexAttribArray(aColor);
 
-    gl.clearColor(1.0, 0.65, 0.0, 1.0); // Merah, Hijau, Biru, Transparansi
+    gl.clearColor(0.75, 0.75, 0.8, 1.0); // Merah, Hijau, Biru, Transparansi
     gl.clear(gl.COLOR_BUFFER_BIT);
     
-    gl.drawArrays(gl.TRIANGLE_FAN, 0, 4);
+    gl.drawArrays(gl.LINE_LOOP, 0, 8);
+    gl.drawArrays(gl.LINE_LOOP, 8, 4);
+
+    gl.drawArrays(gl.LINE_LOOP, 12, 15);
+
 }
