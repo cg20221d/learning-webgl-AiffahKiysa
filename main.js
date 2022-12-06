ce4ac81ec74dba3a968076cc6d6c210d02ecd1da
Aiffah Kiysa Waafi <aiffahwaafi6@gmail.com>
2022-12-06 21:42:45 +0700

    Ambient Light

--- main.js
+++ main.js
@@ -3,31 +3,66 @@ function main() {
     var gl = kanvas.getContext("webgl");
 
     var vertices = [
-        // 0.1, 0.5,  1.0, 1.0, 0.0,
-        // -0.25, 0.5,  0.7, 0.0, 1.0,
-        // -0.25, 0.2, 0.1, 1.0, 0.6,
-        // 0.0, 0.2
+        // Face A       // Red
+        -1, -1, -1,     1, 0, 0,    // Index:  0    
+         1, -1, -1,     1, 0, 0,    // Index:  1
+         1,  1, -1,     1, 0, 0,    // Index:  2
+        -1,  1, -1,     1, 0, 0,    // Index:  3
+        // Face B       // Yellow
+        -1, -1,  1,     1, 1, 0,    // Index:  4
+         1, -1,  1,     1, 1, 0,    // Index:  5
+         1,  1,  1,     1, 1, 0,    // Index:  6
+        -1,  1,  1,     1, 1, 0,    // Index:  7
+        // Face C       // Green
+        -1, -1, -1,     0, 1, 0,    // Index:  8
+        -1,  1, -1,     0, 1, 0,    // Index:  9
+        -1,  1,  1,     0, 1, 0,    // Index: 10
+        -1, -1,  1,     0, 1, 0,    // Index: 11
+        // Face D       // Blue
+         1, -1, -1,     0, 0, 1,    // Index: 12
+         1,  1, -1,     0, 0, 1,    // Index: 13
+         1,  1,  1,     0, 0, 1,    // Index: 14
+         1, -1,  1,     0, 0, 1,    // Index: 15
+        // Face E       // Orange
+        -1, -1, -1,     1, 0.5, 0,  // Index: 16
+        -1, -1,  1,     1, 0.5, 0,  // Index: 17
+         1, -1,  1,     1, 0.5, 0,  // Index: 18
+         1, -1, -1,     1, 0.5, 0,  // Index: 19
+        // Face F       // White
+        -1,  1, -1,     1, 1, 1,    // Index: 20
+        -1,  1,  1,     1, 1, 1,    // Index: 21
+         1,  1,  1,     1, 1, 1,    // Index: 22
+         1,  1, -1,     1, 1, 1     // Index: 23
+    ];
 
-        0.5, 0.0, 0.0, 1.0, 1.0,
-        0.0, -0.5, 1.0, 0.0, 1.0,
-        -0.5, 0.0, 1.0, 1.0, 0.0,
-        0.0, 0.5, 1.0, 1.0, 1.0
+    var indices = [
+        0, 1, 2,     0, 2, 3,     // Face A
+        4, 5, 6,     4, 6, 7,     // Face B
+        8, 9, 10,    8, 10, 11,   // Face C
+        12, 13, 14,  12, 14, 15,  // Face D
+        16, 17, 18,  16, 18, 19,  // Face E
+        20, 21, 22,  20, 22, 23   // Face F     
     ];
 
     var buffer = gl.createBuffer();
     gl.bindBuffer(gl.ARRAY_BUFFER, buffer);
     gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(vertices), gl.STATIC_DRAW);
 
+    var indexBuffer = gl.createBuffer();
+    gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, indexBuffer);
+    gl.bufferData(gl.ELEMENT_ARRAY_BUFFER, new Uint16Array(indices), gl.STATIC_DRAW);
+
     // Vertex shader
     var vertexShaderCode = `
-    attribute vec2 aPosition;
+    attribute vec3 aPosition;
     attribute vec3 aColor;
     varying vec3 fragColor;
     uniform mat4 uModel;
+    uniform mat4 uView;
+    uniform mat4 uProjection;
     void main(){
         fragColor = aColor;
-        vec2 position = aPosition;
-        gl_Position = uModel * vec4(position, 0.0, 1.0);
+        gl_Position = uProjection * uView * uModel * vec4(aPosition, 1.0);
     }
     `;
 
@@ -39,8 +74,12 @@ function main() {
     var fragmentShaderCode = `
     precision mediump float;
     varying vec3 fragColor;
+    uniform vec3 uAmbientConstant;      // merepresentasikan warna sumber cahaya
+    uniform float uAmbientIntensity;    // merepresentasikan intensitas cahaya sekitar
     void main() {
-        gl_FragColor = vec4(fragColor, 1.0);
+        vec3 ambient = uAmbientConstant * uAmbientIntensity;
+        vec3 phong = ambient;
+        gl_FragColor = vec4(phong * fragColor, 1.0);
     }
     `;
     var fragmentShaderObject = gl.createShader(gl.FRAGMENT_SHADER);
@@ -57,10 +96,16 @@ function main() {
     // untuk setiap vertex yang sedang diproses
     var aPosition = gl.getAttribLocation(shaderProgram, "aPosition");
     var aColor = gl.getAttribLocation(shaderProgram, "aColor");
-    gl.vertexAttribPointer(aPosition, 2, gl.FLOAT, false, 5 * Float32Array.BYTES_PER_ELEMENT, 0);
-    gl.vertexAttribPointer(aColor, 3, gl.FLOAT, false, 5 * Float32Array.BYTES_PER_ELEMENT, 2 * Float32Array.BYTES_PER_ELEMENT);
+    gl.vertexAttribPointer(aPosition, 3, gl.FLOAT, false, 6 * Float32Array.BYTES_PER_ELEMENT, 0);
+    gl.vertexAttribPointer(aColor, 3, gl.FLOAT, false, 6 * Float32Array.BYTES_PER_ELEMENT, 3 * Float32Array.BYTES_PER_ELEMENT);
     gl.enableVertexAttribArray(aPosition);
     gl.enableVertexAttribArray(aColor);
+
+    // untuk pencahayaan dan pembayangan
+    var uAmbientConstant = gl.getUniformLocation(shaderProgram, "uAmbientConstant");
+    var uAmbientIntensity = gl.getUniformLocation(shaderProgram, "uAmbientIntensity");
+    gl.uniform3fv(uAmbientConstant, [1.0, 0.5, 1.0]);   // warna sumber cahaya : orange
+    gl.uniform1f(uAmbientIntensity, 0.4);               // intensitas cahaya : 40%
 
     // Variabel lokal
     var theta = 0.0;
@@ -71,6 +116,21 @@ function main() {
 
     // Variabel pointer ke GLSL
     var uModel = gl.getUniformLocation(shaderProgram, "uModel");
+    // View
+    var cameraX = 0.0;
+    var cameraZ = 5.0;
+    var uView = gl.getUniformLocation(shaderProgram, "uView");
+    var view = glMatrix.mat4.create();
+    glMatrix.mat4.lookAt(
+        view,
+        [cameraX, 0.0, cameraZ],    // the location of the eye or the camera
+        [cameraX, 0.0, -10],        // the point where the camera look at
+        [0.0, 1.0, 0.0]
+    );
+    // Projection
+    var uProjection = gl.getUniformLocation(shaderProgram, "uProjection");
+    var perspective = glMatrix.mat4.create();
+    glMatrix.mat4.perspective(perspective, Math.PI/3, 1.0, 0.5, 10.0);
 
     // grafik ai nteraktif
     var freeze = false;
@@ -98,11 +158,13 @@ function main() {
     document.addEventListener("keydown", onKeyDown, false);
 
     function render(){
-        gl.clearColor(0.75, 0.75, 0.8, 1.0); // Merah, Hijau, Biru, Transparansi
-        gl.clear(gl.COLOR_BUFFER_BIT);
+        gl.enable(gl.DEPTH_TEST);
+        // gl.clearColor(0.75, 0.75, 0.8, 1.0); // Merah, Hijau, Biru, Transparansi
+        gl.clearColor(0.0, 0.0, 0.0, 1.0); // hitam
+        gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT);
 
         if (!freeze) {
-            theta += 0.01;
+            theta += 0.05;
         }
         horizontalPoints += horizontal;
         verticalPoints -= vertical;
@@ -110,11 +172,20 @@ function main() {
         glMatrix.mat4.translate(
             model, model, [horizontalPoints, verticalPoints, 0.0]
         );
+        glMatrix.mat4.rotateX(
+            model, model, theta
+        );
+        glMatrix.mat4.rotateY(
+            model, model, theta
+        );
         glMatrix.mat4.rotateZ(
             model, model, theta
         );
         gl.uniformMatrix4fv(uModel, false, model);
-        gl.drawArrays(gl.TRIANGLE_FAN, 0, 4);
+        // gl.drawArrays(gl.TRIANGLE_FAN, 0, 4);
+        gl.uniformMatrix4fv(uView, false, view);
+        gl.uniformMatrix4fv(uProjection, false, perspective);
+        gl.drawElements(gl.TRIANGLES, indices.length, gl.UNSIGNED_SHORT, 0);
         requestAnimationFrame(render);
     }
     // setInterval(render, 1000/60)
